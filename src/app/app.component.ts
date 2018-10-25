f15477b6a48b330efa57442a4ab5645e96fa13e5
desara <desara.kacani@w2020.eu>
2018-10-25 17:02:18 +0200

    add loading icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,27 @@ export class AppComponent implements OnInit {
     public users = [];
     public totalPages = [];
     public currentUser;
+    loadingTable = false;
 
     constructor(private _usersService: UsersService) {
     }
 
     ngOnInit() {
+        this.loadingTable = true;
         this._usersService.getUsers(1)
             .subscribe(users => {
                 this.users = users.data;
                 this.totalPages = Array.apply(null, {length: users.total_pages}).map(Number.call, Number);
+                this.loadingTable = false;
             });
     }
 
     getUsers(page) {
+        this.loadingTable = true;
         this._usersService.getUsers(page).subscribe(
             users => {
                 this.users = users.data;
+                this.loadingTable = false;
             }
         )
     }
